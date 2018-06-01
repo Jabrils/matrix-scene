5c5795a81a4e4c24a1d7833e78bfe683b09b7d8b
Jabrils <ankelzspam@gmail.com>
2018-05-31 22:44:05 -0700

    Initial commit

---
+++ Assets/CamPlay.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamPlay : MonoBehaviour {
+    float cycle;
+    float max = 60;
+    bool begin;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            begin = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+
+        if (begin)
+        {
+            if (cycle < Mathf.PI)
+            {
+                cycle += Time.deltaTime / 3;
+            }
+        }
+
+        Camera.main.fieldOfView = Mathf.Abs(Mathf.Cos(cycle))*max;
+        Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, max*.15f, max);
+
+    }
+}
---
+++ MyCustomNode.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor.ShaderGraph;
+using System.Reflection;
+
+[Title("Custom", "My Custom Node")]
+public class MyCustomNode : CodeFunctionNode
+{
+    public MyCustomNode()
+    {
+        name = "My Custom Node";
+    }
+
+    protected override MethodInfo GetFunctionToConvert()
+    {
+        return GetType().GetMethod("MyCustomFunction",
+            BindingFlags.Static | BindingFlags.NonPublic);
+    }
+
+    static string MyCustomFunction(
+        [Slot(0, Binding.None)] DynamicDimensionVector A,
+        [Slot(1, Binding.None)] DynamicDimensionVector B,
+        [Slot(2, Binding.None)] DynamicDimensionVector C,
+        [Slot(3, Binding.None)] out DynamicDimensionVector Out)
+    {
+        return
+            @"
+{
+	Out = (A * B) * C;
+} 
+";
+    }
+}
